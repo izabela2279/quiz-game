04a1946afe722f063b02e59815e7015e9c035f4a
Izabela <izabelaszeremeta@gmail.com>
2022-12-19 16:56:15 -0500

    Update true and false buttons

--- js/app.js
+++ js/app.js
@@ -30,8 +30,10 @@ const playBtn = document.getElementById("play")
 const nextBtn = document.getElementById("next")
 const resetBtn = document.getElementById("restart")
 const questionEls = document.getElementById("question-display")
+// const answerEls = document.getElementsByClassName("answer")
 const trueBtn = document.getElementById("true-btn")
 const falseBtn = document.getElementById("false-btn")
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -41,33 +43,84 @@ nextBtn.addEventListener("click", nextQuestion)
 
 resetBtn.addEventListener("click", startOver)
 
-trueBtn.addEventListener("click", function(){
+// trueBtn.addEventListener("click", function(){
+//   if (response === quizQuestions.Answer)
+//   return true
+// })
 
-})
+// falseBtn.addEventListener("click", function(){
+//   if (response === quizQuestions.Answer)
+//   return true
+// })
 
-falseBtn.addEventListener("click", function(){
+// answerEls.addEventListener("click")
 
-})
+trueBtn.addEventListener("click", responseTrue)
+
+falseBtn.addEventListener("click", responseFalse)
 
 
 /*-------------------------------- Functions --------------------------------*/
+function init(){
+  questionEls
+  // question = 0;
+  // answer = 0;
+  render()
+}
+init()
 
 function render(){
-  
+  nextQuestion()
 }
 
 function playGame(){
-  questionEls.textContent = quizQuestions[0].Question;
-}
+  questionEls.textContent = quizQuestions[0].Question; {
+    answer = true
+    if (answer === quizQuestions[0].Answer){
+      return ("Correct Answer");
+    } else {
+      return ("Incorrect Answer");
+    }
+  }
+  }
 
 function nextQuestion(){
-  questionEls.textContent = quizQuestions[1].Question;
+  console.log(nextQuestion);
+  quizQuestions.forEach(function(que,idx){
+    questionEls[0].textContent = que
+  });
+  // questionEls.textContent = quizQuestions[1].Question;
+
+   // for (let i = 1; i < quizQuestions.length; i++){
+  //   if (answer === Question[i].Answer[i]){
+  //     return ("Correct Answer");
+  //   } else {
+  //     return ("Incorrect Answer");
+  //   }
+  // }
 }
 
-function response(){
-  
+
+function responseTrue(){
+  answer = true
+  if (answer === quizQuestions.Answer){
+    return "Answer is correct"
+  } else {
+    return "Answer is incorrect"
+  }
+}
+
+function responseFalse(){
+  console.log("Response");
+  answer = false
+  console.log(answer);
+  if (answer === quizQuestions.Answer){
+    return "Answer is correct"
+  } else {
+    return "Answer is incorrect"
+  }
 }
 
 function startOver(){
-  questionEls.textContent
+  init();
 }
