f73d52cd9fd87994a04410ec6e4d268b8f115e18
Izabela <izabelaszeremeta@gmail.com>
2022-12-19 22:47:31 -0500

    Add data file

---
+++ data/questions.js
@@ -0,0 +1,88 @@
+const quizOneQuestions = [
+  {Question: "Are Arabica, Robusta, Liberica and Espresso all coffee beans?",
+    Answers: ["True", "False"],
+    Answer: "False"
+  },
+  {Question: "Is instant coffee made from coffee beans?",
+    Answers: ["True", "False"],
+    Answer: "True"
+  },
+  {Question: "Arabica beans are the most popular beans.",
+    Answers: ["True", "False"],
+    Answer: "True"
+  },
+  {Question: "Not all coffee beans contain caffeine.",
+    Answers: ["True", "False"],
+    Answer: "False"
+  },
+]
+
+const quizTwoQuestions = [
+  {Question: "A French Press requires ground beans",
+    Answers: ["True", "False"],
+    Answer: "True"
+  },
+  {Question: "All espresso machines grind beans",
+    Answers: ["True", "False"],
+    Answer: "False"
+  },
+  {Question: "Pour Over uses a filter",
+    Answers: ["True", "False"],
+    Answer: "True"
+  },
+  {Question: "Cold Brew is not a brewing method",
+    Answers: ["True", "False"],
+    Answer: "False"
+  },
+]
+
+const quizThreeQuestions = [
+  {Question: "Espresso can be served hot or cold",
+    Answers: ["True", "False"],
+    Answer: "True"
+  },
+  {Question: "Flat White has microfoam milk",
+    Answers: ["True", "False"],
+    Answer: "True"
+  },
+  {Question: "Cappucino originated in the US",
+    Answers: ["True", "False"],
+    Answer: "False"
+  },
+  {Question: "A Latte has steamed milk",
+    Answers: ["True", "False"],
+    Answer: "True"
+  },
+]
+
+const quizFourQuestions = [
+  {Question: "Iced Coffee is made using espresso",
+    Answers: ["True", "False"],
+    Answer: "False"
+  },
+  {Question: "It's best to soak ground beans for 24 hours",
+    Answers: ["True", "False"],
+    Answer: "True"
+  },
+  {Question: "Require whole beans instead of ground beans",
+    Answers: ["True", "False"],
+    Answer: "False"
+  },
+  {Question: "Iced coffee can be served with milk",
+    Answers: ["True", "False"],
+    Answer: "True"
+  },
+]
+
+export {
+  quizOneQuestions,
+}
+export {
+  quizTwoQuestions,
+}
+export {
+  quizThreeQuestions,
+}
+export {
+  quizFourQuestions,
+}
--- js/app.js
+++ js/app.js
@@ -1,80 +1,8 @@
 /*-------------------------------- Constants --------------------------------*/
-const quizOneQuestions = [
-  {Question: "Are Arabica, Robusta, Liberica and Espresso all coffee beans?",
-    Answers: ["True", "False"],
-    Answer: "False"
-  },
-  {Question: "Is instant coffee made from coffee beans?",
-    Answers: ["True", "False"],
-    Answer: "True"
-  },
-  {Question: "Arabica beans are the most popular beans.",
-    Answers: ["True", "False"],
-    Answer: "True"
-  },
-  {Question: "Not all coffee beans contain caffeine.",
-    Answers: ["True", "False"],
-    Answer: "False"
-  },
-]
-
-const quizTwoQuestions = [
-  {Question: "A French Press requires ground beans",
-    Answers: ["True", "False"],
-    Answer: "True"
-  },
-  {Question: "All espresso machines grind beans",
-    Answers: ["True", "False"],
-    Answer: "False"
-  },
-  {Question: "Pour Over uses a filter",
-    Answers: ["True", "False"],
-    Answer: "True"
-  },
-  {Question: "Cold Brew is not a brewing method",
-    Answers: ["True", "False"],
-    Answer: "False"
-  },
-]
-
-const quizThreeQuestions = [
-  {Question: "Espresso can be served hot or cold",
-    Answers: ["True", "False"],
-    Answer: "True"
-  },
-  {Question: "Flat White has microfoam milk",
-    Answers: ["True", "False"],
-    Answer: "True"
-  },
-  {Question: "Cappucino originated in the US",
-    Answers: ["True", "False"],
-    Answer: "False"
-  },
-  {Question: "A Latte has steamed milk",
-    Answers: ["True", "False"],
-    Answer: "True"
-  },
-]
-
-const quizFourQuestions = [
-  {Question: "Iced Coffee is made using espresso",
-    Answers: ["True", "False"],
-    Answer: "False"
-  },
-  {Question: "It's best to soak ground beans for 24 hours",
-    Answers: ["True", "False"],
-    Answer: "True"
-  },
-  {Question: "Require whole beans instead of ground beans",
-    Answers: ["True", "False"],
-    Answer: "False"
-  },
-  {Question: "Iced coffee can be served with milk",
-    Answers: ["True", "False"],
-    Answer: "True"
-  },
-]
-  
+import {quizOneQuestions} from "../data/questions.js";
+import {quizTwoQuestions} from "../data/questions.js";
+import {quizThreeQuestions} from "../data/questions.js";
+import {quizFourQuestions} from "../data/questions.js";
 
 /*---------------------------- Variables (state) ----------------------------*/
 let questionOne, questionTwo, questionThree, questionFour, answer, score, response
